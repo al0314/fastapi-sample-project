17aa6943b4483a7c18509ef92ae62e593e4e9ea8
al0314 <alaminmdtanvir@gmail.com>
2025-03-20 23:22:37 +0600

    Feature test#3

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI with Celery and Dockered Composed test"}
+    return {"message": "FastAPI with Celery and Dockered Composed testin"}
 
 @app.post("/tasks/{data}")
 def run_task(data: str):
