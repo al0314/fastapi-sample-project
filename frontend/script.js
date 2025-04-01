00ff112a75695b09e4f7944fb4e05aaf66b921b4
al0314 <alaminmdtanvir@gmail.com>
2025-04-02 00:25:34 +0600

    Add css and javascript

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+window.onload = async () => {
+    const response = await fetch('http://localhost:9000');  // Adjust this based on your FastAPI endpoint
+    const data = await response.json();
+    document.getElementById('message').innerText = data.message || 'Failed to fetch data';
+  };
+  
